876a0e01d5053436ec7ce3dc3fb02dd3fbfd5be1
nikeshadhikari9 <nickeshadhikari@gmail.com>
2023-12-20 09:21:28 +0545

    first

---
+++ Javascript Exercise/datatypes.js
@@ -0,0 +1,17 @@
+let name="Nikesh Adhikari";
+let love=false;
+let a=undefined;
+let b=null;
+let num=56;
+const fname="Nikesh";
+const lname="Adhikari";
+const status="unmarried";
+const country="Nepal";
+let age=21;
+let array=['nikesh','Adhikari','unmarried','nepal',21];
+let myAge=21;
+let yourAge=21.666;
+console.log(typeof name, typeof love, typeof a, typeof b, typeof num);
+console.log(array)
+console.log("I am "+myAge+" years old.")
+console.log("You are "+yourAge+" years old.")
